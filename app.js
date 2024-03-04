45ce04cc5e3615e027f870192de50810d0898f3d
kathy Del Castillo <kathydcastillo90@gmail.com>
2024-03-04 11:39:42 -0500

    First commit

---
+++ app.js
@@ -0,0 +1,9 @@
+function function1(){
+    document.body.style.background = 'pink';
+}
+function function2(){
+    document.body.style.background = "lightgreen";
+}
+function function3(){
+    document.body.style.background = "lightblue";
+}
